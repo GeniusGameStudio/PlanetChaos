461c8c2f4ede3ac26c23933866be07758d08852e
zzxzzk115 <954294627@qq.com>
2020-07-04 22:19:14 +0800

    added cinemachine support

---
+++ ProjectCyberpunk/Assets/Scripts/Mono/Camera/CameraBackground.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBackground : MonoBehaviour
+{
+    public Camera followCamera;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(followCamera.transform.position.x, followCamera.transform.position.y, transform.position.z);
+    }
+}
---
+++ ProjectCyberpunk/Assets/Scripts/Mono/Camera/VCamController.cs
@@ -0,0 +1,30 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VCamController : MonoBehaviour
+{
+    private CinemachineVirtualCamera vcam;
+
+    private float mousewheel;
+
+    public float scaleRate = 0.99f;
+
+    void Start()
+    {
+        vcam = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousewheel = Input.GetAxis("Mouse ScrollWheel");
+
+        if (mousewheel < 0)
+            vcam.m_Lens.OrthographicSize /= scaleRate;
+        else if (mousewheel > 0)
+            vcam.m_Lens.OrthographicSize *= scaleRate;
+        
+    }
+}
