bb1cc2b2ed94530ee38051b9ac61e9abe385be62
keqi lai <469139390@qq.com>
2020-07-05 18:00:00 +0800

    first

---
+++ ProjectCyberpunk/Assets/Scripts/Selectmap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selectmap : MonoBehaviour
+{
+    Transform map;
+
+    private void Start()
+    {
+        Camera cam = map.parent.gameObject.GetComponent<Camera>();
+    }
+    void Update()
+    {
+        map.
+    }
+}
---
+++ ProjectCyberpunk/Assets/Scripts/cameramove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameramove : MonoBehaviour
+{
+    private Vector3 offset;
+    public Transform player;
+
+
+    void Start()
+    {
+        offset = player.position - transform.position;
+    }
+
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.position - offset, Time.deltaTime * 5);
+        Quaternion rotation = Quaternion.LookRotation(player.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 3f);
+    }
+}
