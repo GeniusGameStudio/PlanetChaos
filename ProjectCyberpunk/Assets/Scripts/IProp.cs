8ca0ab7a29aea6bc1b363377b99e684bfe2f3282
yuanxzzz <2689299788@qq.com>
2020-07-08 00:50:39 +0800

    Menu

---
+++ ProjectCyberpunk/Assets/Scripts/IProp.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IProp
+{
+    //爆炸
+    void Explode();
+    //对物体经行销毁
+    void Destory();
+}
---
+++ ProjectCyberpunk/Assets/yxz/Scripts/Menu/MouseEnter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+//鼠标指向按钮有放大效果
+public class MouseEnter : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject button;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        button.transform.localScale = new Vector3(1.2f, 1.2f, 1f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        button.transform.localScale = new Vector3(1f, 1f, 1f);
+    }
+}
